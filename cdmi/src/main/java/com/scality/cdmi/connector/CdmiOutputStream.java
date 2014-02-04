9dff1cefc58886f2ec704fcdd861eff842761e1d
ziad.bizri@ezako.com <ziad.bizri@ezako.com>
2014-02-04 14:37:58 +0100

    Flush in non cdmi mode when writing in non cdmi mode

--- cdmi/src/main/java/com/scality/cdmi/connector/CdmiOutputStream.java
+++ cdmi/src/main/java/com/scality/cdmi/connector/CdmiOutputStream.java
@@ -218,7 +218,7 @@ public class CdmiOutputStream extends OutputStream {
 					new ThreadPoolExecutor.CallerRunsPolicy());
 		}
 
-		connector.forceFlushCdmi(path);
+		connector.forceFlushNonCdmi(path);
 	}
 
 	/**
